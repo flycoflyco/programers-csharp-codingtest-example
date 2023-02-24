0be4639bc6670a34edf19da45a3dd1c3d9524738
flycoflyco <flycoflyco@naver.com>
2023-02-24 16:08:08 +0900

    [Bronze V] Title: ??!, Time: 68 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp34
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string inputStr = Console.ReadLine();
+			Console.WriteLine(inputStr + "??!");
+		}
+	}
+}
