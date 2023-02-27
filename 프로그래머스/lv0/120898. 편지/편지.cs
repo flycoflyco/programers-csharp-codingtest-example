0af8df0712ff9ab6acfe4d4b2a9801ba4f9a1629
flycoflyco <flycoflyco@naver.com>
2023-02-27 16:37:03 +0900

    [level 0] Title: 편지, Time: 0.14 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(string message) 
+    {
+        int answer = 0;
+        answer = message.Length * 2;
+        return answer;
+    }
+}
