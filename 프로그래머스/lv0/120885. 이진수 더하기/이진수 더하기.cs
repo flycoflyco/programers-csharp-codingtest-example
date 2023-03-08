5f36c309c6adf951f613188d5afa42977e2c4dde
flycoflyco <flycoflyco@naver.com>
2023-03-08 17:18:14 +0900

    [level 0] Title: 이진수 더하기, Time: 1.11 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+public class Solution {
+
+		public string solution(string bin1, string bin2)
+		{
+			string answer = "";
+			int num1 = GetDec(bin1);
+			int num2 = GetDec(bin2);
+			answer = GetBin(num1 + num2);
+			return answer;
+		}
+
+		public static int GetDec(string bin)
+		{
+			int result = 0;
+
+			char[] binArr = bin.ToCharArray();
+			int raise = 0; // 지수부
+
+			for (int i = binArr.Length-1; i >= 0; i--)
+			{
+				char ch = binArr[i];
+				if (ch == '1')
+				{
+					if (raise == 0)
+					{
+						result += 1;
+					}
+					else
+					{
+						int n = 1;
+						for (int j = 0; j < raise; j++)
+						{
+							n *= 2;
+						}
+						
+						result += n;
+
+					}
+				}
+				raise++;
+			}
+
+			return result;
+		}
+
+		public string GetBin(int decNum)
+		{
+			string result = string.Empty;
+
+			if (decNum == 0)
+				return "0";
+
+			
+			int mok = decNum;
+			int margin = 0;
+			while(true)
+			{
+				margin = mok % 2;
+				result += margin.ToString();
+				mok = mok / 2;
+
+				if (mok == 1)
+				{
+					result += "1";
+					break;
+				}
+			}
+
+			int strIndex = 0;
+			char[] charray = new char[result.Length];
+			for (int i = result.Length-1; i >= 0; i--)
+			{
+				char ch = result[i];
+				charray[strIndex] = ch;
+				strIndex++;
+			}
+
+			result = new string(charray);
+			
+			return result;
+		}
+}
