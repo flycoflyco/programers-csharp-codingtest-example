2b3cab080f5c57cf15cfed6833d3e8996edd8730
flycoflyco <flycoflyco@naver.com>
2023-03-15 14:28:28 +0900

    [level 1] Title: 평균 구하기, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public double solution(int[] arr) {
+			double answer = 0;
+			int sum = 0;
+			for (int i = 0; i < arr.Length; i++)
+			{
+				sum += arr[i];
+			}
+			answer = (double)sum / (double)arr.Length;
+			return answer;
+    }
+}
