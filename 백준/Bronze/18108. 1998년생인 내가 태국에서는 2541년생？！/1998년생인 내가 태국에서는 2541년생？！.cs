1633df0e6883c8f4f0d83ee50c5ad99f20bf2536
flycoflyco <flycoflyco@naver.com>
2023-03-19 10:00:40 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace Programers_codetest_example
+{
+	internal class Program
+	{
+        public static void Main()
+		{
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            int output = num - 543;
+            Console.WriteLine(output);
+        }
+    }
+}
