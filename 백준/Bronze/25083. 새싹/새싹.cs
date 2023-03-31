bf16c8aaa9e9347c4ed97786b0aaebf30474e39b
flycoflyco <flycoflyco@naver.com>
2023-03-31 13:25:13 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp34
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			// string line1 = "         ,r'\"7";
+			Console.WriteLine("         ,r'\"7");
+			Console.WriteLine("r`-_   ,'  ,/");
+			Console.WriteLine(" \\. \". L_r'");
+			Console.WriteLine("   `~\\/");
+			Console.WriteLine("      |");
+			Console.WriteLine("      |");
+
+			int a = 0;
+
+		}
+	}
+}
+
