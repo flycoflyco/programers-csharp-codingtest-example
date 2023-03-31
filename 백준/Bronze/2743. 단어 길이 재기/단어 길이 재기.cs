77c461dafb900f0b3f38f5531cdefa6844ed7e36
flycoflyco <flycoflyco@naver.com>
2023-03-31 11:49:52 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 60 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp34
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string s = Console.ReadLine();
+			Console.Write(s.Length);
+		}
+	}
+}
+
