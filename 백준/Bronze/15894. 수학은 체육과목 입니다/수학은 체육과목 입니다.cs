31359cae549e1e363dc4fc528c002f6a3ca785be
flycoflyco <flycoflyco@naver.com>
2023-04-10 11:08:03 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace ConsoleApp34
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string inputStr = Console.ReadLine();
+			ulong input = Convert.ToUInt64(inputStr);
+			Console.WriteLine(input * 4);
+		}
+	}
+}
+
