6735794fa5281be50aabd6b48bd15d23aacbc9b8
flycoflyco <flycoflyco@naver.com>
2023-04-10 09:52:55 +0900

    [Bronze V] Title: 직사각형, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp34
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string inputStr1 = Console.ReadLine();
+			string inputStr2 = Console.ReadLine();
+
+			int input1 = Convert.ToInt32(inputStr1);
+			int input2 = Convert.ToInt32(inputStr2);
+
+			Console.WriteLine(input1 * input2);
+
+		}
+	}
+}
+
