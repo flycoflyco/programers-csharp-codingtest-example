ad99303f1609004b4b860ab3f778ec7fb943b3da
flycoflyco <flycoflyco@naver.com>
2023-04-16 09:06:18 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 64 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Programers_codetest_example
+{
+	public  class Program
+	{
+		public static void Main()
+		{
+			string inputByStr = Console.ReadLine();
+			ulong inputNum = Convert.ToUInt64(inputByStr);
+
+			Console.WriteLine(inputNum* inputNum* inputNum);
+			Console.WriteLine(3);
+
+		}
+
+
+
+
+	}
+}
