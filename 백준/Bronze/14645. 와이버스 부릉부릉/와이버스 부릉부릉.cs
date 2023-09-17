2d9e17a19d63dd13646470b2f10002029c08df70
flycoflyco <flycoflyco@naver.com>
2023-09-17 12:02:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 68 ms, Memory: 5540 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,27 @@
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputSplit = input.Split(' ');
+            int n = int.Parse(inputSplit[0]);
+            int k = int.Parse(inputSplit[1]);
+            
+            for (int i = 0; i < n; i++)
+            {
+                string cmd = Console.ReadLine();
+            }
+            Console.WriteLine("비와이");
+        }
+    }
+}
+
+
+
+
