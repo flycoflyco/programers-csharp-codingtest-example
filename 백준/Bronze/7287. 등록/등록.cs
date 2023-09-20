c918bc1bcbd3ea66d6f2c63e7a0cfbed28e73c94
flycoflyco <flycoflyco@naver.com>
2023-09-20 09:27:14 +0900

    [Bronze V] Title: 등록, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("216");
+            Console.WriteLine("flycoflyco");
+        }
+    }
+}
