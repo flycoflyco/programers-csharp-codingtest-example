ef0983c11e2a9ee6a62525a15d5442a5020bad72
flycoflyco <flycoflyco@naver.com>
2023-09-26 22:34:13 +0900

    [Bronze V] Title: A+B - 2, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
