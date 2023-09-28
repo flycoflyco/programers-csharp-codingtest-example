754b81288519e3bfb98a487e64ca09c615cec90c
flycoflyco <flycoflyco@naver.com>
2023-09-28 09:01:51 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 56 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            string temp = @"       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|";
+            Console.WriteLine(temp);
+        }
+    }
+}
