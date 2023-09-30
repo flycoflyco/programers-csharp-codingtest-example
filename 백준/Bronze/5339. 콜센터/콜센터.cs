ac7a5336010570686454f4d899535d5ecf9cf389
flycoflyco <flycoflyco@naver.com>
2023-09-30 15:52:11 +0900

    [Bronze V] Title: 콜센터, Time: 56 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            string temp = @"     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |";
+
+            Console.WriteLine(temp);
+        }
+    }
+}
