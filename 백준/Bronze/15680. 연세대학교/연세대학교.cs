295f64c5e9e596fb82a79b6a5f27dbbadd9a615b
flycoflyco <flycoflyco@naver.com>
2023-10-03 12:32:32 +0900

    [Bronze V] Title: 연세대학교, Time: 60 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if ( input == 0)
+            {
+                Console.WriteLine("YONSEI");
+            }
+            else
+            {
+                Console.WriteLine("Leading the Way to the Future");
+            }
+        }
+    }
+}
